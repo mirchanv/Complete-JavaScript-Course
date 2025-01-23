4f893bcd79db541bf0e114719adec833019b06e7
Vishal Michandani <137283854+mirchanv@users.noreply.github.com>
2025-01-23 10:50:59 +0100

    Update Challenge.js

--- 01-Fundamentals-Part-1/Challenges/02. Data Types/Challenge.js
+++ 01-Fundamentals-Part-1/Challenges/02. Data Types/Challenge.js
@@ -1,5 +1,4 @@
 // Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
-
 // Log the types of isIsland and language to the console.
 
 let isIsland = false;
